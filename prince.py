1e1f4b75e512ce3d656f0a33d4ac2c67833e69af
Rob <robertplee@live.com>
2018-03-15 23:44:25 +0000

    Updated library to something that works in Python3 albeit slightly hackily

--- prince.py
+++ prince.py
@@ -1,38 +1,78 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from bitstring import BitArray
+#from bitstring import BitArray
+from binascii import unhexlify, hexlify
 
 class Prince:
-    RC = (BitArray(hex = '0x0000000000000000'),
-          BitArray(hex = '0x13198a2e03707344'),
-          BitArray(hex = '0xa4093822299f31d0'),
-          BitArray(hex = '0x082efa98ec4e6c89'),
-          BitArray(hex = '0x452821e638d01377'),
-          BitArray(hex = '0xbe5466cf34e90c6c'),
-          BitArray(hex = '0x7ef84f78fd955cb1'),
-          BitArray(hex = '0x85840851f1ac43aa'),
-          BitArray(hex = '0xc882d32f25323c54'),
-          BitArray(hex = '0x64a51195e0e3610d'),
-          BitArray(hex = '0xd3b5a399ca0c2399'),
-          BitArray(hex = '0xc0ac29b7c97c50dd'))
+    RC = (unhexlify('0000000000000000'),
+          unhexlify('13198a2e03707344'),
+          unhexlify('a4093822299f31d0'),
+          unhexlify('082efa98ec4e6c89'),
+          unhexlify('452821e638d01377'),
+          unhexlify('be5466cf34e90c6c'),
+          unhexlify('7ef84f78fd955cb1'),
+          unhexlify('85840851f1ac43aa'),
+          unhexlify('c882d32f25323c54'),
+          unhexlify('64a51195e0e3610d'),
+          unhexlify('d3b5a399ca0c2399'),
+          unhexlify('c0ac29b7c97c50dd'))
 
-    S = ('0xb', '0xf', '0x3', '0x2', '0xa', '0xc', '0x9', '0x1', 
-         '0x6', '0x7', '0x8', '0x0', '0xe', '0x5', '0xd', '0x4')
+#    S = ('0xb', '0xf', '0x3', '0x2', '0xa', '0xc', '0x9', '0x1',
+#         '0x6', '0x7', '0x8', '0x0', '0xe', '0x5', '0xd', '0x4')
 
-    Sinv = ('0xb', '0x7', '0x3', '0x2', '0xf', '0xd', '0x8', '0x9', 
-            '0xa', '0x6', '0x4', '0x0', '0x5', '0xe', '0xc', '0x1')
+    S = ([1,0,1,1], [1,1,1,1], [0,0,1,1], [0,0,1,0], [1,0,1,0], [1,1,0,0], [1,0,0,1], [0,0,0,1],
+         [0,1,1,0], [0,1,1,1], [1,0,0,0], [0,0,0,0], [1,1,1,0], [0,1,0,1], [1,1,0,1], [0,1,0,0])
+
+#    Sinv = ('0xb', '0x7', '0x3', '0x2', '0xf', '0xd', '0x8', '0x9',
+#            '0xa', '0x6', '0x4', '0x0', '0x5', '0xe', '0xc', '0x1')
+
+    Sinv = ([1,0,1,1], [0,1,1,1], [0,0,1,1], [0,0,1,0], [1,1,1,1], [1,1,0,1], [1,0,0,0], [1,0,0,1],
+            [1,0,1,0], [0,1,1,0], [0,1,0,0], [0,0,0,0], [0,1,0,1], [1,1,1,0], [1,1,0,0], [0,0,0,1])
+
+    from_nib = ([0,0,0,0], [0,0,0,1], [0,0,1,0], [0,0,1,1], [0,1,0,0], [0,1,0,1], [0,1,1,0], [0,1,1,1],
+                [1,0,0,0], [1,0,0,1], [1,0,1,0], [1,0,1,1], [1,1,0,0], [1,1,0,1], [1,1,1,0], [1,1,1,1])
+
+    #shift_rows = (00, 05, 10, 15, 04, 09, 14, 03, 08, 13, 02, 07, 12, 01, 06, 11)
+
+    def list_xor(self, l1, l2):
+      output = []
+      if len(l1) != len(l2):
+        print("ERROR list xor: input lists of unequal length")
+        quit()
+      for i in range(0, len(l1)):
+        output.append(l1[i] ^ l2[i])
+      return output
+
+
+    def make_bit_array(self, data):
+      out = []
+
+      for i in range(0, len(data)):
+        temp = data[i]
+        byte = []
+        for j in range(0, 8):
+          byte = [temp & 1] + byte
+          temp = temp >> 1
+        out = out + byte
+
+      return out
 
 
     def sbox(self, data, box):
-        ret = BitArray()
-        for nibble in data.cut(4):
-            ret.append(box[int(nibble.hex, 16)])
+        ret = []
+        #for nibble in data.cut(4):
+            #ret.append(box[int(nibble.hex, 16)])
+        i = 0
+        while i < len(data):
+          nib = ((((((data[i] << 1) + data[i+1]) << 1) + data[i+2]) << 1) + data[i+3])
+          ret += box[nib]
+          i += 4
         return ret
 
 
     def m0(self, data):
-        ret = BitArray(length = 16)
+        ret = [0] * 16
         ret[ 0] = data[4] ^ data[ 8] ^ data[12]
         ret[ 1] = data[1] ^ data[ 9] ^ data[13]
         ret[ 2] = data[2] ^ data[ 6] ^ data[14]
@@ -53,7 +93,7 @@ class Prince:
 
 
     def m1(self, data):
-        ret = BitArray(length = 16)
+        ret = [0] * 16
         ret[ 0] = data[0] ^ data[ 4] ^ data[ 8]
         ret[ 1] = data[5] ^ data[ 9] ^ data[13]
         ret[ 2] = data[2] ^ data[10] ^ data[14]
@@ -74,19 +114,38 @@ class Prince:
 
 
     def shiftrows(self, data, inverse):
-        ret = BitArray(length = 64)
+        #ret = BitArray(length = 64)
+        ret = [0] * 64
         idx = 0
-        for nibble in data.cut(4):
-            ret[idx * 4:(idx + 1) * 4] = nibble
-            if not inverse:
-                idx = (idx + 13) % 16
-            else:
-                idx = (idx +  5) % 16
+        i = 0
+        while i < len(data):
+          #nib = ((((((data[i] << 1) + data[i+1]) << 1) + data[i+2]) << 1) + data[i+3])
+          nib = data[i:i+4]
+          ret[idx * 4:(idx + 1) * 4] = nib
+          if not inverse:
+            idx = (idx + 13) % 16
+          else:
+            idx = (idx +  5) % 16
+          i += 4
+
+        #for nibble in data.cut(4):
+            #ret[idx * 4:(idx + 1) * 4] = nibble
+            #if not inverse:
+                #idx = (idx + 13) % 16
+            #else:
+                #idx = (idx +  5) % 16
+
+        #out = []
+        #for i in range(0, len(ret)):
+          #out += Prince.from_nib[ret[i]]
+        #return out
         return ret
 
 
+
     def mprime(self, data):
-        ret = BitArray(length = 64)
+        #ret = BitArray(length = 64)
+        ret = [0] * 64
         ret[ 0:16] = self.m0(data[ 0:16])
         ret[16:32] = self.m1(data[16:32])
         ret[32:48] = self.m1(data[32:48])
@@ -99,13 +158,17 @@ class Prince:
             data = self.sbox(data, Prince.S)
             data = self.mprime(data)
             data = self.shiftrows(data, inverse = False)
-            data ^= Prince.RC[idx] ^ key
+            #data ^= Prince.RC[idx] ^ key
+            round_key = self.list_xor(key, self.make_bit_array(Prince.RC[idx]))
+            data = self.list_xor(data, round_key)
         return data
 
 
     def lastrounds(self, data, key):
         for idx in (6,7,8,9,10):
-            data ^= key ^ Prince.RC[idx]
+            #data ^= key ^ Prince.RC[idx]
+            round_key = self.list_xor(key, self.make_bit_array(Prince.RC[idx]))
+            data = self.list_xor(data, round_key)
             data = self.shiftrows(data, inverse = True)
             data = self.mprime(data)
             data = self.sbox(data, Prince.Sinv)
@@ -113,7 +176,11 @@ class Prince:
 
 
     def princecore(self, data, key):
-        data ^= key ^ Prince.RC[0]
+        #data ^= key ^ Prince.RC[0]
+        first_round_key = self.list_xor(key, self.make_bit_array(Prince.RC[0]))
+        #print("xored key and rc[0]")
+        data = self.list_xor(first_round_key, data)
+        #print("xored first_round_key and data")
         data = self.firstrounds(data, key)
 
         data = self.sbox(data, Prince.S)
@@ -121,33 +188,75 @@ class Prince:
         data = self.sbox(data, Prince.Sinv)
 
         data = self.lastrounds(data, key)
-        return data ^ key ^ Prince.RC[11]
+        final_round_key = self.list_xor(key, self.make_bit_array(Prince.RC[11]))
+        return self.list_xor(data, final_round_key)
+        #return data ^ key ^ Prince.RC[11]
+
+    def list_rotate_right(self, l, num):
+      output = list(l)
+
+      for i in range(0, len(l)):
+        if i < num:
+          output[i] = l[len(l) - num + i]
+        else:
+          output[i] = l[i - num]
+      return output
 
 
     def outer(self, data, key, decrypt = False):
         k0 = key[0:64]
-        k0prime = k0.copy()
-        k0prime.ror(1)
-        k0prime ^= k0 >> 63
+        #k0prime = list(k0)
+        #k0prime.ror(1)
+        #k0prime ^= k0 >> 63
+        k0prime = self.list_rotate_right(k0, 1)
+        k0prime[len(k0prime) - 1] = k0prime[len(k0prime) - 1] ^ k0prime[1]
+
         if decrypt:
-            tmp = k0
-            k0 = k0prime
-            k0prime = tmp
+            tmp = list(k0)
+            k0 = list(k0prime)
+            k0prime = list(tmp)
         k1 = key[64:128]
 
-        data = k0 ^ data                                # pre-whitening
+        #data = k0 ^ data                                # pre-whitening
+        data = self.list_xor(k0, data)                        # pre-whitening
         data = self.princecore(data, k1)
-        return (data ^ k0prime).hex                     # post-whitening
+        ret = self.list_xor(data, k0prime)
+        conv = []
+        i = 0
+        while i < (len(ret) - 0):
+          out = 0
+          for j in range(0, 8):
+            out = (out << 1) + ret[i+j]
+          conv += [out]
+          i += 8
+
+        return (bytes(conv))                     # post-whitening
 
 
     def encrypt(self, plaintext, key):
-        bitkey = BitArray('0x' + key.encode('hex'))
-        bittext = BitArray('0x' + plaintext.encode('hex'))
-        return self.outer(bittext, bitkey).decode('hex')
+        bitkey = self.make_bit_array(unhexlify(key))
+        bittext = self.make_bit_array(unhexlify(plaintext))
+        #print(bitkey)
+        #print(bittext)
+        #return self.outer(bittext, bitkey)
+        #return hexlify(self.outer(bittext, bitkey))
+        res = hexlify(self.outer(bittext, bitkey))
+        ret = ""
+        for i in range(0, len(res)):
+          ret += chr(res[i])
+        return ret
+
 
 
     def decrypt(self, ciphertext, key):
-        bitkey = BitArray('0x' + key.encode('hex'))
-        bitkey ^= "0x0000000000000000c0ac29b7c97c50dd"  # alpha padded with zero
-        bittext = BitArray('0x' + ciphertext.encode('hex'))
-        return self.outer(bittext, bitkey, True).decode('hex')
+        bitkey = self.make_bit_array(unhexlify(key))
+        bittext = self.make_bit_array(unhexlify(ciphertext))
+        #bitkey = BitArray('0x' + key.encode('hex'))
+        bitkey = self.list_xor(bitkey, self.make_bit_array(unhexlify("0000000000000000c0ac29b7c97c50dd")))  # alpha padded with zero
+        #bittext = BitArray('0x' + ciphertext.encode('hex'))
+        res = hexlify(self.outer(bittext, bitkey, True))
+        ret = ""
+        for i in range(0, len(res)):
+          ret += chr(res[i])
+        return ret
+        #return hexlify(self.outer(bittext, bitkey, True))
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from binascii import hexlify, unhexlify
+
 from prince import Prince
 import os
 
@@ -8,30 +10,32 @@ import os
 def test(ptxt, key, exp):
     cipher = Prince()
     ctxt = cipher.encrypt(ptxt, key)
+    #print(exp)
+    #print(ctxt)
     if ctxt != exp:
-        print "FAILED encryption of {} with key {}. Expected: {}, got: {}".format(ptxt.encode('hex'), key.encode('hex'), exp.encode('hex'), ctxt.encode('hex'))
+        print("FAILED encryption of {" + ptxt + "} with key {" + key + "}. Expected: {" + exp + "}, got: {" + ctxt + "}")
     else:
-        print "PASSED encryption of {} with key {}. Yields:   {}".format(ptxt.encode('hex'), key.encode('hex'), ctxt.encode('hex'))
+        print("PASSED encryption of {" + ptxt + "} with key {" + key + "}. Yields:   {" + exp + "}")
     dec = cipher.decrypt(ctxt, key)
     if dec != ptxt:
-        print "FAILED decryption of {} with key {}. Expected: {}, got: {}".format(ctxt.encode('hex'), key.encode('hex'), ptxt.encode('hex'), dec.encode('hex'))
+        print("FAILED decryption of {" + ctxt + "} with key {" + key + "}. Expected: {" + ptxt + "}, got: {" + dec + "}")
     else:
-        print "PASSED decryption of {} with key {}. Yields:   {}".format(ctxt.encode('hex'), key.encode('hex'), dec.encode('hex'))
+        print("PASSED decryption of {" + ctxt + "} with key {" + key + "}. Yields:   {" + dec + "}")
 
 
 if __name__ == "__main__":
     cipher = Prince()
-    print "Running test1"
-    test("\x00" * 8, "\x00" * 16, "818665aa0d02dfda".decode('hex'))
-    
-    print "\nRunning test2"
-    test("\xff" * 8, "\x00" * 16, "604ae6ca03c20ada".decode('hex'))
-    
-    print "\nRunning test3"
-    test("\x00" * 8, "\xff" * 8 + "\x00" * 8, "9fb51935fc3df524".decode('hex'))
-    
-    print "\nRunning test4"
-    test("\0" * 8, "\x00" * 8 + "\xff" * 8, "78a54cbe737bb7ef".decode('hex'))
+    print("Running test1")
+    test("00" * 8, "00" * 16, "818665aa0d02dfda")
 
-    print "\nRunning test5"
-    test("0123456789abcdef".decode('hex'), "\x00" * 8 + "fedcba9876543210".decode('hex'), "ae25ad3ca8fa9ccf".decode('hex'))
+    print("\nRunning test2")
+    test("ff" * 8, "00" * 16, "604ae6ca03c20ada")
+
+    print("\nRunning test3")
+    test("00" * 8, "ff" * 8 + "00" * 8, "9fb51935fc3df524")
+
+    print("\nRunning test4")
+    test("00" * 8, "00" * 8 + "ff" * 8, "78a54cbe737bb7ef")
+
+    print("\nRunning test5")
+    test("0123456789abcdef", "00" * 8 + "fedcba9876543210", "ae25ad3ca8fa9ccf")
